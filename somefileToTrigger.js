e04d219fffd86a8b0e15e3d5c562c0c7315bf4ae
Martin Krastev <martooo1936@gmail.com>
2023-07-17 20:44:22 +0000

    new file expecting to trigger

---
+++ somefileToTrigger.js
@@ -0,0 +1,80 @@
+// send a get request to microsoft.learn module
+// 
+// // Path: somefileToTrigger.js
+// // import axios
+// const axios = require('axios');
+// 
+// // make GET request to microsoft.learn module
+// axios.get('https://docs.microsoft.com/api/learn/catalog')
+//   .then(response => {
+//     // handle success
+//     console.log(response);
+//   })
+//   .catch(error => {
+//     // handle error
+//     console.log(error);
+//   })
+//   .then(() => {
+//     // always executed
+//   });
+// send a post request to microsoft.learn module
+// 
+// // Path: somefileToTrigger.js
+// // import axios
+// const axios = require('axios');
+// 
+// // make POST request to microsoft.learn module
+// axios.post('https://docs.microsoft.com/api/learn/catalog', {
+//   firstName: 'Fred',
+//   lastName: 'Flintstone'
+// })
+//   .then(response => {
+//     // handle success
+//     console.log(response);
+//   })
+//   .catch(error => {
+//     // handle error
+//     console.log(error);
+//   })
+//   .then(() => {
+//     // always executed
+//   });
+// send a put request to microsoft.learn module
+// 
+// // Path: somefileToTrigger.js
+// // import axios
+// const axios = require('axios');
+// 
+// // make PUT request to microsoft.learn module
+// axios.put('https://docs.microsoft.com/api/learn/catalog', {
+//   firstName: 'Fred',
+//   lastName: 'Flintstone'
+// })
+//   .then(response => {
+//     // handle success
+//     console.log(response);
+//   })
+//   .catch(error => {
+//     // handle error
+//     console.log(error);
+//   })
+//   .then(() => {
+//     // always executed
+//   });
+// send a delete request to microsoft.learn module
+// 
+// // Path: somefileToTrigger.js
+// // import axios
+// const axios = require('axios');
+// 
+// // make DELETE request to microsoft.learn module
+// axios.delete('https://docs.microsoft.com/api/learn/catalog')
+//   .then(response => {
+//     // handle success
+//     console.log(response);
+//   })
+//   .catch(error => {
+//     // handle error
+//     console.log(error);
+//   })
+//
